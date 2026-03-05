6c0b78f8b08e1eeac1bf5efde06404b928df14e7
victorjuniorlanadasilva-cyber <victorjuniorlanadasilva@gmail.com>
2026-03-05 09:07:37 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker instalado!');
-});
-
-self.addEventListener('fetch', function(event) {
-  // Aqui poderia colocar cache, mas por enquanto deixa vazio
-});
